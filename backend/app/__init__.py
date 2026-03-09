a9fe3be4448dc16d039f667b77773ccc488a2a4c
Alejandro Perez <alejandro.perez21@upr.edu>
2026-03-08 23:03:18 -0400

    Added `Patient` CRUD Endpoints and fixed `schema` error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,6 +7,7 @@ from config import config
 # Initialize extensions (not yet bound to an app)
 db = SQLAlchemy()
 
+
 def create_app(config_name="development"):
     app = Flask(__name__)
 
@@ -24,9 +25,9 @@ def create_app(config_name="development"):
         level=logging.DEBUG,
         format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
         handlers=[
-            logging.StreamHandler(),              # prints to terminal
-            logging.FileHandler("app.log")        # saves to app.log file
-        ]
+            logging.StreamHandler(),  # prints to terminal
+            logging.FileHandler("app.log"),  # saves to app.log file
+        ],
     )
     app.logger.info("ClinicHUB Flask app initialized")
 
@@ -34,7 +35,10 @@ def create_app(config_name="development"):
     # Register blueprints (routes)
     # -------------------------------------------------------------------------
     from app.routes import main
+    from app.routes.patients import patients_bp
+
     app.register_blueprint(main)
+    app.register_blueprint(patients_bp)
 
     # -------------------------------------------------------------------------
     # Basic error handlers
