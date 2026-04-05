a292a7cd2b2a3c3042cecaf45a20d36e46e0f714
Cristian Barreras Tatsenko <cristian.barreras@upr.edu>
2026-04-04 20:15:21 -0400

    added code model to test the reports and basic reports test

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.patient import Patient
 from app.models.role import Role
 from app.models.user import User
 from app.models.cpt import CPT
+from app.models.cpt_code import CPTCode
 
 __all__ = ["Patient", "Role", "User"]
